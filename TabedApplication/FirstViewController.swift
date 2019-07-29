cc38109e9ecc8c364fb71858f71793c9c1b06f7f
Sebastian Zus <sebastian.zus@endava.com>
2019-07-29 07:22:54 +0300

    Initial Commit

---
+++ TabedApplication/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabedApplication
+//
+//  Created by Sebastian Zus on 29/07/2019.
+//  Copyright © 2019 Endava. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
